b921257a371fc53d3fc4aaae4fb60c18f6602c55
Dannydoesdev <50442868+Dannydoesdev@users.noreply.github.com>
2020-10-21 00:09:55 +1100

    finalising all code - buttons working etc (just dropdown broken)

--- js/main.js
+++ js/main.js
@@ -1,42 +1,49 @@
-console.log("hi");
+$(document).ready(function () {
+    $("#banner-close").click(function () {
+        $("#main-banner-id").slideUp("slow");
+    });
 
+    $("#tap-to-open-team").click(function () {
+        $(".your-team").slideToggle("slow");
+    });
 
+    $("#tap-to-open-bookings").click(function () {
+        $(".bookings-card").slideToggle("slow");
+    });
 
+    $("#tap-to-open-info").click(function () {
+        $(".promo-area").slideToggle("slow");
+    });
 
-// $(document).ready(function () {
-//     $("#banner-close").click(function () {
-//         $("#main-banner-id").slideUp("slow");
-//     });
+    ("#tap-to-open-jobs").click(function () {
+        $("#jobs-created-toggle").slideToggle("slow");
+    });
 
-//     $("#tap-to-open-team").click(function () {
-//         $(".your-team").slideToggle("slow");
-//     });
-
-//     $("#tap-to-open-bookings").click(function () {
-//         $(".bookings-card").slideToggle("slow");
-//     });
-
-//     $("#tap-to-open-info").click(function () {
-//         $(".promo-area").slideToggle("slow");
-//     });
-// });
+});
 
 document.getElementById("hamburger").addEventListener("click", sideMenuAppears);
-document.getElementById('menu-close').addEventListener('click', sideMenuDisappears);
+document.getElementById("menu-close").addEventListener("click", sideMenuDisappears);
 
 
 function sideMenuAppears() {
-    console.log('sideMenuAppears() is active');
-    //  - if you want to test the function, uncomment this
     document.getElementById("side-menu").classList.add("show-menu");
 }
 
-/* When you click menu close, it puts the hide-menu class onto #side-menu, reversing what just happened */
 function sideMenuDisappears() {
-    // console.log('sideMenuDisappears() is active'); - if you want to test the function, uncomment this
-    document.getElementById('side-menu').classList.remove('show-menu');
+    document.getElementById("side-menu").classList.remove("show-menu");
 }
 
+
+// document.getElementById("dropdown-trigger").addEventListener("mouseover", menuOneShow);
+// document.getElementById("menu-1").addEventListener("mouseleave", menusHide);
+// function menuOneShow() {
+//     document.getElementById("menu-1").classList.add("show");
+// }
+
+// function menusHide() {
+//     document.getElementById("menu-1").classList.remove("show");
+
+// }
 
 
 
